02aa989c25055d5f8f7020d701f9eaffe08279bd
Mostafijur Rahaman <148426873+mostafijursd@users.noreply.github.com>
2024-03-24 19:03:56 +0530

    change

--- mrportfolio/src/components/footer/FooterBotton.jsx
+++ mrportfolio/src/components/footer/FooterBotton.jsx
@@ -4,7 +4,7 @@ function FooterBotton() {
   return (
     <div className="w-full py-10">
     <p className="text-center text-gray-500 text-base">
-      © 2022. All rights reserved by  Mostafijur Rahaman
+      © 2024. All rights reserved by  Mostafijur Rahaman
     </p>
   </div>
   )
